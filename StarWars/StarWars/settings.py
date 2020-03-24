0c8c3a4f1b32d039e50df955dd6c5cb9d6090a7e
olga1110 <a11os@yandex.ru>
2020-03-24 19:22:48 +0300

    fix

--- StarWars/StarWars/settings.py
+++ StarWars/StarWars/settings.py
@@ -131,7 +131,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'StarWars', 'static'),)
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
